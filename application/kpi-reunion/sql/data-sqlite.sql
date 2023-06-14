bfa617a608f849451ddb51d7cab09adfc1debd99
hmtmcse <hmtm.cse@gmail.com>
2023-06-14 13:07:45 +0600

    changed

--- application/kpi-reunion/sql/data-sqlite.sql
+++ application/kpi-reunion/sql/data-sqlite.sql
@@ -1,5 +1,5 @@
 
-INSERT INTO event_details (name, adult_fee, child_fee, created, updated, is_deleted, is_default, uuid) VALUES ("কুড়িগ্রাম পলিটেকনিক ইনস্টিটিউট পুনর্মিলনী - ২০২৩", 750, 600, strftime('%Y-%m-%d %H:%M:%S','now'), strftime('%Y-%m-%d %H:%M:%S','now'), 0, 1, '80B2E23A-7E38-11ED-A7BB-8X2AFD1190BD');
+INSERT INTO event_details (name, adult_fee, child_fee, created, updated, is_deleted, is_default, uuid) VALUES ("রংপুর পলিটেকনিক ইনস্টিটিউট পুনর্মিলনী - ২০২৩", 750, 600, strftime('%Y-%m-%d %H:%M:%S','now'), strftime('%Y-%m-%d %H:%M:%S','now'), 0, 1, '80B2E23A-7E38-11ED-A7BB-8X2AFD1190BD');
 
 UPDATE member SET access_type = "Admin" WHERE mobile = "01756702340";
 UPDATE member SET access_type = "Admin" WHERE mobile = "01717743019";
