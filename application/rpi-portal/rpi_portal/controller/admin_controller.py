09a9979c9f8e0362e8dd743d0506b2041533eb06
hmtmcse <hmtm.cse@gmail.com>
2023-06-15 14:16:04 +0600

    changed

--- application/rpi-portal/rpi_portal/controller/admin_controller.py
+++ application/rpi-portal/rpi_portal/controller/admin_controller.py
@@ -1,4 +1,5 @@
 from flask import Blueprint
+from rpi_portal.service.member_service import MemberService
 
 url_prefix = "/admin"
 admin_controller = Blueprint(
@@ -6,3 +7,20 @@ admin_controller = Blueprint(
     __name__,
     url_prefix=url_prefix
 )
+
+member_service = MemberService()
+
+
+@admin_controller.route("/member-list", methods=['GET'])
+def member_list():
+    return member_service.member_list()
+
+
+@admin_controller.route("/member-details/<int:id>", methods=['GET'])
+def member_details(id: int):
+    return member_service.member_details(id)
+
+
+@admin_controller.route("/reset/<int:id>", methods=['POST', 'GET'])
+def reset(id: int):
+    return member_service.reset(id)
