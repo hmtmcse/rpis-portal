a848710183479be4a9913785d2f7c43c0442b18e
@Rakibuzzamanneon <rakib@problemfighter.com>
2023-06-15 14:42:27 +0600

    icon and dashboard data change

--- application/rpi-portal/rpi_portal/common/navigation_data.py
+++ application/rpi-portal/rpi_portal/common/navigation_data.py
@@ -17,12 +17,12 @@ class NavigationData:
         navigation_list.append(NavigationData.get_nav_item("member_controller.dashboard", "Dashboard", "fas fa-chart-pie"))
 
         if RPIAuthUtil.is_student():
-            navigation_list.append(NavigationData.get_nav_item("member_controller.mark_sheet", "Mark Sheet", "fa-solid fa-headset"))
-            navigation_list.append(NavigationData.get_nav_item("member_controller.certificate", "Certificate", "fa-solid fa-headset"))
-            navigation_list.append(NavigationData.get_nav_item("member_controller.protoyon_potro", "প্রত্যয়ন পত্র","fa-solid fa-headset"))
+            navigation_list.append(NavigationData.get_nav_item("member_controller.mark_sheet", "Mark Sheet", "fa-solid fa-file-lines"))
+            navigation_list.append(NavigationData.get_nav_item("member_controller.certificate", "Certificate", "fa-solid fa-certificate"))
+            navigation_list.append(NavigationData.get_nav_item("member_controller.protoyon_potro", "প্রত্যয়ন পত্র","fa-solid fa-file"))
 
         if RPIAuthUtil.is_admin():
-            navigation_list.append(NavigationData.get_nav_item("admin_controller.member_list", "Members", "fa-solid fa-headset"))
+            navigation_list.append(NavigationData.get_nav_item("admin_controller.member_list", "Members", "fa-solid fa-rectangle-list"))
 
         navigation_list.append(NavigationData.get_nav_item("member_controller.profile", "Profile", "fa-brands fa-canadian-maple-leaf"))
         navigation_list.append(NavigationData.get_nav_item("member_controller.support", "Support", "fa-solid fa-headset"))
