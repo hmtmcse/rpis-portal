cba27d54ccd0c339567686369e862a218809ef06
@Rakibuzzamanneon <rakib@problemfighter.com>
2023-06-16 21:26:29 +0600

    resolve and icon change name change

--- application/rpi-portal/rpi_portal/common/navigation_data.py
+++ application/rpi-portal/rpi_portal/common/navigation_data.py
@@ -22,15 +22,15 @@ class NavigationData:
             navigation_list.append(NavigationData.get_nav_item("member_controller.protoyon_potro", "প্রত্যয়ন পত্র","fa-solid fa-file"))
 
         if RPIAuthUtil.is_admin():
-            navigation_list.append(NavigationData.get_nav_item("admin_controller.student_list", "Student", "fa-solid fa-rectangle-list"))
-            navigation_list.append(NavigationData.get_nav_item("admin_controller.operator_list", "Operator", "fa-solid fa-rectangle-list"))
+            navigation_list.append(NavigationData.get_nav_item("admin_controller.student_list", "Student", "fa-solid fa-graduation-cap"))
+            navigation_list.append(NavigationData.get_nav_item("admin_controller.operator_list", "Operator", "fa-solid fa-shield-heart"))
 
         if RPIAuthUtil.is_register():
-            navigation_list.append(NavigationData.get_nav_item("register_controller.registration_approval", "Registration", "fa-solid fa-rectangle-list"))
-            navigation_list.append(NavigationData.get_nav_item("register_controller.registration_approval", "প্রত্যয়ন পত্র", "fa-solid fa-rectangle-list"))
-            navigation_list.append(NavigationData.get_nav_item("register_controller.mark_sheet", "Mark Sheet", "fa-solid fa-rectangle-list"))
-            navigation_list.append(NavigationData.get_nav_item("register_controller.certificate", "Certificate", "fa-solid fa-rectangle-list"))
-            navigation_list.append(NavigationData.get_nav_item("register_controller.receive_request", "Receive Request", "fa-solid fa-rectangle-list"))
+            navigation_list.append(NavigationData.get_nav_item("register_controller.registration_approval", "Registration", "fa-solid fa-file-circle-check"))
+            navigation_list.append(NavigationData.get_nav_item("register_controller.registration_approval", "প্রত্যয়ন পত্র", "fa-solid fa-file"))
+            navigation_list.append(NavigationData.get_nav_item("register_controller.mark_sheet", "Mark Sheet", "fa-solid fa-file-lines"))
+            navigation_list.append(NavigationData.get_nav_item("register_controller.certificate", "Certificate", "fa-solid fa-certificate"))
+            navigation_list.append(NavigationData.get_nav_item("register_controller.receive_request", "Receive Request", "fa-solid fa-bars-progress"))
 
         navigation_list.append(NavigationData.get_nav_item("member_controller.profile", "Profile", "fa-brands fa-canadian-maple-leaf"))
         navigation_list.append(NavigationData.get_nav_item("member_controller.support", "Support", "fa-solid fa-headset"))
